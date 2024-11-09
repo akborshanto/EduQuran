3108a4781f50a424a50a5151a91ecd70f8ae4870
AKBOR SHANTO <akborshanto11111@gmail.com>
2024-11-09 02:03:42 -0800

    top student sention

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { Banner } from '../../component/Banner'
+import { OurTeacher } from '../../component/OurTeacher'
+import TopStudent from '../../component/TopStudent'
 
 export const Home = () => {
   return (
-    <div><Banner></Banner></div>
+    <div><Banner></Banner>
+    <OurTeacher></OurTeacher>
+    <TopStudent></TopStudent>
+    </div>
   )
 }
