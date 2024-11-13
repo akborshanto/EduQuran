9ba9acb1143c8f4525b3f634328e4040d2d3d405
AKBOR SHANTO <akborshanto11111@gmail.com>
2024-11-13 12:11:25 +0600

    clock compnent

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -3,11 +3,15 @@ import { Banner } from '../../component/Banner'
 import { OurTeacher } from '../../component/OurTeacher'
 import TopStudent from '../../component/TopStudent'
 import NewsLetter from '../../component/NewsLetter'
+import StudentProfile from '../../component/NewsAnnouncement'
 
 export const Home = () => {
   return (
-    <div><Banner></Banner>
+    <div>
  
+      <Banner></Banner>
+<StudentProfile></StudentProfile>
+ <NewsLetter></NewsLetter>
     </div>
   )
 }
